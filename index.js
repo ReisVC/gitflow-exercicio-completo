04aee2b9331f3e85144e5bba3adb6f9e29e35e1e
ReisVC <vitor.reisvc@gmail.com>
2025-05-26 14:12:19 -0300

    fix(somar): corrige lógica de somar na release

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 // Projeto GitFlow completo
 
 function somar(a, b) {
-    return a - b;
+    return a + b;
   }
   console.log('Soma:', somar(2, 3));
   
 
   function multiplicar(a, b) {
-    return a * 2;
+    return a / 2;
   }
   console.log('Multiplicação:', multiplicar(4, 5));
   
